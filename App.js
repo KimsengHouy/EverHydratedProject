0cbdc3bcca48a2368f4c8596994c5e92270e9b07
KimsengHouy <1809170001@students.stamford.edu>
2021-12-03 14:10:18 +0700

    init

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+
+import AuthStackNavigator from './src/navigation/AuthStackNavigator';
+import AppStack from './src/navigation/AppStack';
+import HomeStackNavigator from './src/navigation/HomeStackNavigator';
+import auth from '@react-native-firebase/auth';
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const onAuthStateChanged = async user => {
+    await setCurrentUser(user);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+  return (
+    <NavigationContainer style={styles.root}>
+      {currentUser ? <HomeStackNavigator /> : <AuthStackNavigator />}
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: '#D1F4FA',
+  },
+});
+
+export default App;
